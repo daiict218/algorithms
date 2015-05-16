f2b626ee17833dff86a3367fd9497263e8adabe8
Ajay Gaur <daiict218@gmail.com>
2015-05-17 02:04:20 +0530

    After making 3 insert functions

--- LinkedList.java
+++ LinkedList.java
@@ -33,11 +33,65 @@ class node
 
 class linkedlist
 {
-	node head;
+	public node head;
 	
 	public linkedlist()	
 	{
 		this.head = null;
+	}
+		
+	public node getHead()
+	{
+		return head;
+	}
+
+	public void insertAtHead(int data)
+	{
+		node nodetoadd = new node(data);
+		if(head == null)
+		{
+			head = nodetoadd;
+		}
+		else
+		{
+			nodetoadd.setNext(head);
+			head = nodetoadd;
+		}
+	}
+
+	public void insertAfterNode(node n,int data)
+	{
+		node nodetoadd = new node(data);
+		nodetoadd.setNext(n.getNext());
+		n.setNext(nodetoadd);
+	}
+
+	public void insertAtEnd(int data)
+	{
+		node nodetoadd = new node(data);
+		if(head == null)
+		{
+			head = nodetoadd;
+		}
+		else
+		{
+			node temp = head;
+			while(temp.getNext()!=null)
+			{
+				temp = temp.getNext();
+			}
+			temp.setNext(nodetoadd);
+		}
+	}
+
+	public void print()
+	{
+		node temp = head;
+		while(temp != null)
+		{
+			System.out.println(temp.getData());
+			temp = temp.getNext();
+		}
 	}
 }
 
@@ -46,6 +100,13 @@ class main
 	public static void main(String args[])
 	{
 		Scanner input = new Scanner(System.in);
-		
+		linkedlist list = new linkedlist();
+		list.insertAtEnd(1);
+		list.insertAtEnd(2);
+		list.insertAtEnd(3);
+		list.insertAtHead(4);
+		list.insertAtHead(5);
+		list.insertAfterNode(list.head,6);
+		list.print();
 	}
 }
